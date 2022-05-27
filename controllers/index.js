e44c87f6c91baa880daca9cd17ebd8eaba63c172
daregoodness1997 <daregoodness@gmail.com>
2022-05-27 19:28:17 +0100

    controller api finalized

--- controllers/index.js
+++ controllers/index.js
@@ -2,38 +2,67 @@ const Task = require('../models/Task');
 
 const getTasks = async (req, res) => {
   try {
-    res.status(200).json('get all Tasks');
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks });
   } catch (err) {
-    console.log(err);
+    res.status(500).json({ msg: err });
   }
 };
 const createTask = async (req, res) => {
   try {
     const task = await Task.create(req.body);
-    res.status(200).json({ task });
+    res.status(201).json({ task });
   } catch (err) {
-    console.log(err);
+    res.status(500).json({ msg: err });
   }
 };
 const getTask = async (req, res) => {
   try {
-    res.status(200).json('get Task');
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ msg: `Task with ${taskID} not found` });
+    }
+    res.status(200).json({ task });
   } catch (err) {
-    console.log(err);
+    res.status(500).json({ msg: err });
   }
 };
 const updateTask = async (req, res) => {
   try {
-    res.status(200).json('Task updated');
+    const { id: taskID } = req.params;
+    const body = req.body;
+
+    const task = await Task.findOneAndUpdate({ _id: taskID }, body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!task) {
+      return res.status(404).json({ msg: `Task with ${taskID} not found` });
+    }
+    res.status(200).json({
+      status: 'success',
+      msg: `Task with ${taskID}  updated`,
+      task,
+    });
   } catch (err) {
-    console.log(err);
+    res.status(500).json({ msg: err });
   }
 };
 const deleteTask = async (req, res) => {
   try {
-    res.status(200).json('Task deleted');
+    const { id: taskID } = req.params;
+    const task = await Task.findByIdAndDelete({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ msg: `Task with ${taskID} not found` });
+    }
+    res.status(200).json({
+      msg: `Task with ${taskID} deleted`,
+      task: null,
+      status: 'success',
+    });
   } catch (err) {
-    console.log(err);
+    res.status(500).json({ msg: err });
   }
 };
 
